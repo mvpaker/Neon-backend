eb8a2bc6cb4b5a94eb69d0d5487be89933493ac1
udghatan-dev <114804720+MernOnly@users.noreply.github.com>
2022-10-20 15:26:15 +0530

    CORS Enabled for All

--- server.js
+++ server.js
@@ -23,14 +23,7 @@ const PORT = process.env["PORT"] || 5000;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(
-    cors({
-        origin: [
-            'https://neon-l.com'
-        ],
-        credentials: true
-    })
-);
+app.use(cors({ origin: '*' }));
 
 
 // app.use(function (req, res, next) {
